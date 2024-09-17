cf51776621d654aa54efc35a5d2f12e21971df0e
Andassa <andassaramananandro@gmail.com>
2024-09-17 10:39:01 +0300

    first commit

---
+++ EmployeeManagementSystem/Dashboard.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace EmployeeManagementSystem
+{
+    partial class Dashboard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.dashboard_IE = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dashboard_AE = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dashboard_TE = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(17, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(844, 176);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(8)))), ((int)(((byte)(138)))));
+            this.panel5.Controls.Add(this.dashboard_IE);
+            this.panel5.Controls.Add(this.label6);
+            this.panel5.Controls.Add(this.pictureBox3);
+            this.panel5.Location = new System.Drawing.Point(573, 27);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(236, 123);
+            this.panel5.TabIndex = 2;
+            // 
+            // dashboard_IE
+            // 
+            this.dashboard_IE.AutoSize = true;
+            this.dashboard_IE.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboard_IE.ForeColor = System.Drawing.Color.White;
+            this.dashboard_IE.Location = new System.Drawing.Point(193, 27);
+            this.dashboard_IE.Name = "dashboard_IE";
+            this.dashboard_IE.Size = new System.Drawing.Size(23, 25);
+            this.dashboard_IE.TabIndex = 6;
+            this.dashboard_IE.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(90, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Inactive Employees";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::EmployeeManagementSystem.Properties.Resources.icons8_group_background_selected_60px;
+            this.pictureBox3.Location = new System.Drawing.Point(20, 27);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(8)))), ((int)(((byte)(138)))));
+            this.panel4.Controls.Add(this.dashboard_AE);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.pictureBox2);
+            this.panel4.Location = new System.Drawing.Point(302, 27);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(236, 123);
+            this.panel4.TabIndex = 1;
+            // 
+            // dashboard_AE
+            // 
+            this.dashboard_AE.AutoSize = true;
+            this.dashboard_AE.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboard_AE.ForeColor = System.Drawing.Color.White;
+            this.dashboard_AE.Location = new System.Drawing.Point(194, 27);
+            this.dashboard_AE.Name = "dashboard_AE";
+            this.dashboard_AE.Size = new System.Drawing.Size(23, 25);
+            this.dashboard_AE.TabIndex = 4;
+            this.dashboard_AE.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(91, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Active Employees";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::EmployeeManagementSystem.Properties.Resources.icons8_group_background_selected_filled_60px;
+            this.pictureBox2.Location = new System.Drawing.Point(16, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(8)))), ((int)(((byte)(138)))));
+            this.panel3.Controls.Add(this.dashboard_TE);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Location = new System.Drawing.Point(32, 27);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(236, 123);
+            this.panel3.TabIndex = 0;
+            // 
+            // dashboard_TE
+            // 
+            this.dashboard_TE.AutoSize = true;
+            this.dashboard_TE.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboard_TE.ForeColor = System.Drawing.Color.White;
+            this.dashboard_TE.Location = new System.Drawing.Point(196, 27);
+            this.dashboard_TE.Name = "dashboard_TE";
+            this.dashboard_TE.Size = new System.Drawing.Size(23, 25);
+            this.dashboard_TE.TabIndex = 2;
+            this.dashboard_TE.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(93, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total Employees";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::EmployeeManagementSystem.Properties.Resources.icons8_employee_card_60px_1;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.chart2);
+            this.panel2.Controls.Add(this.chart1);
+            this.panel2.Location = new System.Drawing.Point(17, 219);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(844, 324);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // chart1
+            // 
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(474, -1);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(369, 316);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // chart2
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(18, 19);
+            this.chart2.Name = "chart2";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(376, 284);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Dashboard";
+            this.Size = new System.Drawing.Size(875, 565);
+            this.panel1.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label dashboard_IE;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label dashboard_AE;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label dashboard_TE;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
---
+++ EmployeeManagementSystem/Dashboard.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace EmployeeManagementSystem
+{
+    public partial class Dashboard : UserControl
+    {
+        private readonly SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Andassa\Documents\employee.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=False");
+
+        public Dashboard()
+        {
+            InitializeComponent();
+            SetupCharts();
+            RefreshData();
+        }
+
+        private void SetupCharts()
+        {
+            // Configure the pie chart
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+            ChartArea chartArea1 = new ChartArea("MainArea1");
+            chart1.ChartAreas.Add(chartArea1);
+            Series series1 = new Series("StatusDistribution")
+            {
+                ChartType = SeriesChartType.Pie,
+                IsValueShownAsLabel = true
+            };
+            chart1.Series.Add(series1);
+            chart1.Titles.Clear();
+            chart1.Titles.Add("Employee Status Distribution");
+
+            // Configure the bar chart
+            chart2.Series.Clear();
+            chart2.ChartAreas.Clear();
+            ChartArea chartArea2 = new ChartArea("MainArea2");
+            chart2.ChartAreas.Add(chartArea2);
+            Series series2 = new Series("StatusPercentage")
+            {
+                ChartType = SeriesChartType.Bar,
+                IsValueShownAsLabel = true,
+                LabelFormat = "{0}%"
+            };
+            chart2.Series.Add(series2);
+            chart2.Titles.Clear();
+            chart2.Titles.Add("Employee Status Percentages");
+        }
+
+        public void RefreshData()
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)RefreshData);
+                return;
+            }
+
+            displayTE();
+            displayAE();
+            displayIE();
+            UpdateCharts();
+        }
+
+        public void displayTE()
+        {
+            if (connect.State != ConnectionState.Open)
+            {
+                try
+                {
+                    connect.Open();
+                    string selectData = "SELECT COUNT(id) FROM employees WHERE delete_date IS NULL";
+                    using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                    {
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+                        dashboard_TE.Text = count.ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+        }
+
+        public void displayAE()
+        {
+            if (connect.State != ConnectionState.Open)
+            {
+                try
+                {
+                    connect.Open();
+                    string selectData = "SELECT COUNT(id) FROM employees WHERE status = @status AND delete_date IS NULL";
+                    using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@status", "Active");
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+                        dashboard_AE.Text = count.ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+        }
+
+        public void displayIE()
+        {
+            if (connect.State != ConnectionState.Open)
+            {
+                try
+                {
+                    connect.Open();
+                    string selectData = "SELECT COUNT(id) FROM employees WHERE status = @status AND delete_date IS NULL";
+                    using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@status", "Inactive");
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+                        dashboard_IE.Text = count.ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+        }
+
+        private void UpdateCharts()
+        {
+            if (connect.State != ConnectionState.Open)
+            {
+                try
+                {
+                    connect.Open();
+
+                    int total = GetCount("SELECT COUNT(id) FROM employees WHERE delete_date IS NULL");
+                    int active = GetCount("SELECT COUNT(id) FROM employees WHERE status = 'Active' AND delete_date IS NULL");
+                    int inactive = GetCount("SELECT COUNT(id) FROM employees WHERE status = 'Inactive' AND delete_date IS NULL");
+
+                    // Update the pie chart
+                    chart1.Series["StatusDistribution"].Points.Clear();
+                    chart1.Series["StatusDistribution"].Points.AddXY("Active", active);
+                    chart1.Series["StatusDistribution"].Points.AddXY("Inactive", inactive);
+
+                    // Update the bar chart with percentages
+                    double activePercentage = total > 0 ? (active / (double)total) * 100 : 0;
+                    double inactivePercentage = total > 0 ? (inactive / (double)total) * 100 : 0;
+
+                    chart2.Series["StatusPercentage"].Points.Clear();
+                    chart2.Series["StatusPercentage"].Points.AddXY("Active", activePercentage);
+                    chart2.Series["StatusPercentage"].Points.AddXY("Inactive", inactivePercentage);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+        }
+
+        private int GetCount(string query)
+        {
+            int count = 0;
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connect))
+                {
+                    count = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error retrieving count: " + ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return count;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            // Handle chart1 click event if needed
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+            // Handle chart2 click event if needed
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // Ajoutez ici la logique nécessaire pour le dessin de panel1, si nécessaire.
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            // Ajoutez ici la logique nécessaire pour le dessin de panel2, si nécessaire.
+        }
+    }
+}
